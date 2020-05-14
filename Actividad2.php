74d7b764244f7f6ba1e7e05a5caaa5fe6a897231
Giogar-13 <giogar13@comunidad.unam.mx>
2020-05-14 13:09:04 -0500

    Feat : Agregué el archivo Actividad2.php

---
+++ Actividad2.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "Garfias Becerril Giovanni";
+
+echo "He aprendido qué son las cookies, y en verdad me ha gustado porque siempre veía el concepto y nunca supe qué era.";
+
+echo "También los formularios, está asombroso lo que puedes hacer en PHP con los datos de las personas. Y pues el manejo de las variables es súper importante.";
+
+
+
+ ?>
